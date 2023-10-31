c85e1b668524975b55caa36f439af01e3e329bd9
fabi-cm <dahofcmmejia@gmail.com>
2023-10-31 17:39:46 -0400

    views

--- cobro_app/infraestructura/views.py
+++ cobro_app/infraestructura/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 from ..aplicacion.services import PagoServicio
 
-# Create your views here.
+
+# Retornar una respuesta exitosa en la solicitud de la API.
+
+# Se deberá enviar una notificación push al local indicando que el pago fue realizado exitosamente.
+
+# Se deberá enviar un sms al local indicando que el cobro fue realizado exitosamente.
+
+# Se deberá enviar un sms al cliente indicando que el pago fue realizado exitosamente.
+
